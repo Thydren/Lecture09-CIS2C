6d3ba7a6ed764fe1fcf1585fdccdad67e314c253
Gutierrez <brandongutierrez@my.whccd.edu>
2020-02-10 19:55:20 -0800

    Finished Lecture

---
+++ script.js
@@ -0,0 +1,20 @@
+let myGrades = [80, 70, 80, 90, 75];
+
+/*console.log(myGrades[0]);
+console.log(myGrades[4]);
+ going from left to right
+        80, 70, 80, 90, 75 
+index   0,  1,  2,  3,  4
+*/
+
+/* for(let i = 0; i < myGrades.length; i++) {
+    console.log(myGrades[i]);
+}
+*/
+myGrades.push(50);
+console.log(myGrades);
+
+console.log(myGrades.pop());
+
+console.log(myGrades);
+
